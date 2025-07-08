d54687a08f0bd984cb4f2da0079db133e2804814
Derek Novak <derek.novak1@gmail.com>
2025-07-08 11:15:25 -0500

    Changed instrumenation file. Added profileService. Updated config

--- server_main/instrumentation.js
+++ server_main/instrumentation.js
@@ -1,9 +1,22 @@
-const Pyroscope = require('@pyroscope/nodejs');
+const opentelemetry = require('@opentelemetry/sdk-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-grpc');
+const {
+  OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
-Pyroscope.init({
-  serverAddress: 'http://localhost:4040',
-  appName: 'Telemetry-Test-App',
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter(),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
-console.log('Starting Pyroscope Profiler');
-Pyroscope.start();
+console.log('Starting OTLP Instrumentation');
+sdk.start();

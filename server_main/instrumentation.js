3da7c12323ae5e9eaeb30ce90db1e7a1bf77b264
Derek Novak <derek.novak1@gmail.com>
2025-07-13 18:24:50 -0500

    Added Host-Metrics approach. Setup for Beyla eBPF approach. Included Dockerfile and docker-compose for easy containerization.

--- server_main/instrumentation.js
+++ server_main/instrumentation.js
@@ -1,22 +1,46 @@
-const opentelemetry = require('@opentelemetry/sdk-node');
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-  OTLPTraceExporter,
-} = require('@opentelemetry/exporter-trace-otlp-grpc');
-const {
-  OTLPMetricExporter,
-} = require('@opentelemetry/exporter-metrics-otlp-grpc');
-const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+// NODE SDK APPROACH
 
-const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter(),
-  }),
-  instrumentations: [getNodeAutoInstrumentations()],
-});
+// const opentelemetry = require('@opentelemetry/sdk-node');
+// const {
+//   getNodeAutoInstrumentations,
+// } = require('@opentelemetry/auto-instrumentations-node');
+// const {
+//   OTLPTraceExporter,
+// } = require('@opentelemetry/exporter-trace-otlp-grpc');
+// const {
+//   OTLPMetricExporter,
+// } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+// const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
-console.log('Starting OTLP Instrumentation');
-sdk.start();
+// const sdk = new opentelemetry.NodeSDK({
+//   traceExporter: new OTLPTraceExporter(),
+//   metricReader: new PeriodicExportingMetricReader({
+//     exporter: new OTLPMetricExporter(),
+//   }),
+//   instrumentations: [getNodeAutoInstrumentations()],
+// });
+
+// console.log('Starting OTLP Instrumentation');
+// sdk.start();
+
+// HOST-METRICS APPROACH
+
+// const { MeterProvider } = require('@opentelemetry/sdk-metrics');
+// const { HostMetrics } = require('@opentelemetry/host-metrics');
+// const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+
+// const exporter = new PrometheusExporter(
+//   {
+//     startServer: true,
+//   },
+//   () => {
+//     console.log('prometheus scrape endpoint: http://localhost:9464/metrics');
+//   }
+// );
+
+// const meterProvider = new MeterProvider({
+//   readers: [exporter],
+// });
+
+// const hostMetrics = new HostMetrics({ meterProvider });
+// hostMetrics.start();

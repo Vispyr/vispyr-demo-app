885b8c61efae1ba212b427d2ebae98724e302e47
kchelilim <98861797+KipChelilim@users.noreply.github.com>
2025-07-23 13:47:10 -0400

    fixing pyroscope instrumentation config and legacy docker namespacing

--- server_main/profileService.js
+++ server_main/profileService.js
@@ -1,9 +1,9 @@
-const Pyroscope = require('@pyroscope/nodejs');
+const Pyroscope = require("@pyroscope/nodejs");
 
 Pyroscope.init({
-  serverAddress: 'http://pyroscope:4040',
-  appName: 'Telemetry-Test-App',
+  serverAddress: "http://localhost:4040",
+  appName: "Telemetry-Test-App",
 });
 
-console.log('Starting Pyroscope Profiler');
+console.log("Starting Pyroscope Profiler");
 Pyroscope.start();
